32511e80c71d79ed1402ac2dda9e5e6096762545
jp-1990 <plum_1990@hotmail.co.uk>
2021-01-17 13:41:08 +0000

    delete redundant bootstrapping elements from gatsby new

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Educational static site explaining common data structures and algorithms with animations.`,
+    author: `J.Plummer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
